e8ddc83a73976689f433b64fa5a4597942c2cff4
Hendi Santika <hendsantika@yahoo.co.id>
2019-02-04 17:32:24 +0700

    #6 - Modify Main Class

--- src/main/java/com/hendisantika/springaopperformancemeasurement/SpringAopPerformanceMeasurementApplication.java
+++ src/main/java/com/hendisantika/springaopperformancemeasurement/SpringAopPerformanceMeasurementApplication.java
@@ -1,10 +1,27 @@
 package com.hendisantika.springaopperformancemeasurement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/")
 public class SpringAopPerformanceMeasurementApplication {
+
+    @Autowired
+    private JuiceService beerService;
+
+    @RequestMapping("/drink")
+    public String drink() throws InterruptedException {
+        return beerService.drink();
+    }
+
+    @RequestMapping("/order")
+    public String order() throws InterruptedException {
+        return beerService.order();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAopPerformanceMeasurementApplication.class, args);
