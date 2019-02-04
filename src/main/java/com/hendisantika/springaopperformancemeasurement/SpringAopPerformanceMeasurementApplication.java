95b6653a41361a8af49c78f5e044452a4f0e1e2b
Hendi Santika <hendsantika@yahoo.co.id>
2019-02-04 17:25:38 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springaopperformancemeasurement/SpringAopPerformanceMeasurementApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springaopperformancemeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopPerformanceMeasurementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopPerformanceMeasurementApplication.class, args);
+    }
+
+}
+
